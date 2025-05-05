ccc0291b83f385783e629c134f59b9f81f212e2d
uzuw <uzuwxd@gmail.com>
2025-05-05 19:18:59 +0545

    creating frontend application and moving all the backend files to the new directory

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import {Route, Routes} from "react-router-dom"
+
+const App = () => {
+  return (
+    <div>
+      This is the frontend application 
+    </div>
+  )
+}
+
+export default App
