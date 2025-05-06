954145a2fbcca989b2ca474680731ed37a17e60a
uzuw <uzuwxd@gmail.com>
2025-05-06 22:31:12 +0545

    routes and links and make session token handeling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import React from "react"
-import {Route, Routes} from "react-router-dom"
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import RegisterPage from "./pages/RegisterPage";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      This is the frontend application 
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
